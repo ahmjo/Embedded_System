934c041c9a96f38c3fe8621cc9f4fe36b0e9ba50
ahmjo <ahmed15517563@gmail.com>
2024-07-16 15:46:24 +0300

    unit3 (lesson1)

---
+++ assignments/unit3 (Embedded C)/lesson 1/Platform_Types(without stdint).h
@@ -0,0 +1,24 @@
+#ifndef Platfprm_Types.h
+#define Platfprm_Types.h
+
+#typedef signed char         sint8;
+#typedef unsigned char       uint8;
+#typedef char                char_t;
+#typedef signed short        sint16;
+#typedef unsigned short      uint16;
+#typedef signed int          sint32;
+#typedef unsigned int        uint32;
+#typedef long long           sint64;
+#typedef unsigned long long  uint64; 
+
+#typedef volatile signed char         vsint8_t;
+#typedef volatile unsigned char       vuint8_t;
+#typedef volatile signed short        vsint16_t;
+#typedef volatile unsigned short      vuint16_t;
+#typedef volatile signed int          vsint32_t;
+#typedef volatile unsigned int        vuint32_t;
+#typedef volatile long long           vsint64_t;
+#typedef volatile unsigned long long  vuint64_t; 
+
+
+#endif
