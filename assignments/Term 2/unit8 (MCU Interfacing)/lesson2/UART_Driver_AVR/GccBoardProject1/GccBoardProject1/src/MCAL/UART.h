9708d366bcfb8217c9b0f9ca5d275e903f8e874a
unknown <ahmed15517563@gmail.com>
2025-03-13 19:09:30 +0200

    u8 l2 s1

---
+++ assignments/Term 2/unit8 (MCU Interfacing)/lesson2/UART_Driver_AVR/GccBoardProject1/GccBoardProject1/src/MCAL/UART.h
@@ -0,0 +1,18 @@
+﻿#include <../Utils.h>
+#include <stdint.h>
+#include <avr/io.h>
+
+#ifndef UART_H_
+#define UART_H_
+
+
+
+void UART_Init(void);
+void UART_Send(uint8_t data);
+uint8_t UART_Receive(void);
+void UART_Send_String(const char *str);
+void UART_Send_Num(uint32_t num);
+void UART_Receive_Num();
+
+
+#endif /* UART_H_ */
---
+++ assignments/Term 2/unit8 (MCU Interfacing)/lesson2/UART_Driver_AVR/GccBoardProject1/GccBoardProject1/src/main.c
@@ -0,0 +1,23 @@
+#include <HAL/LCD_Driver/LCD.h>
+#include <MCAL/UART.h>
+#include <./Utils.h>
+#include <stdint.h>
+#include <avr/io.h>
+
+int main()
+{
+	LCD_INIT();
+	UART_Init();
+	
+	LCD_GOTO_XY(1,0);
+	LCD_WRITE_STRING("UART Tx");
+	UART_Send('x');
+	UART_Send('y');
+	LCD_GOTO_XY(2,0);
+	while(1)
+	{
+		
+		char x=UART_Receive();
+		LCD_WRITE_CHAR(x);
+	}
+}
